4097b49d5e5e0b79e48855a5c6effd88a88e61bd
Darren <59044608+darrenleeyx@users.noreply.github.com>
2024-04-09 16:32:03 +0800

    Added helper functions to check if string is json and also prettify json string with tests

--- src/Application/Helpers/JsonHelper.cs
+++ src/Application/Helpers/JsonHelper.cs
@@ -1,20 +1,58 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text.Json;
+using System.Text.Json.Nodes;
 
 namespace Application.Helpers;
 
 public static class JsonHelper
 {
-    public static bool IsJson(string value)
+    /// <summary>
+    /// Checks if the provided string is a valid JSON.
+    /// </summary>
+    /// <param name="input">The input string to check.</param>
+    /// <returns>true if the input is valid JSON; otherwise, false.</returns>
+    public static bool IsValidJson(string input)
     {
-        if (string.IsNullOrEmpty(value))
+        if (string.IsNullOrEmpty(input))
         {
             return false;
         }
 
-        return true;
+        input = input.Trim();
+
+        if ((input.StartsWith("{") && input.EndsWith("}")) ||   // For objects
+            (input.StartsWith("[") && input.EndsWith("]")))     // For array
+        {
+            try
+            {
+                var _ = JsonValue.Parse(input);
+                return true;
+            }
+            catch (FormatException)
+            {
+                return false;
+            }
+        }
+
+        return false;
+    }
+
+    /// <summary>
+    /// Formats a JSON string to be more human-readable with indented formatting.
+    /// </summary>
+    /// <param name="input">The JSON string to prettify. If the string is null or empty, the method returns an empty string.</param>
+    /// <returns>A prettified JSON string with indented formatting. If the input is null or empty, returns an empty string.</returns>
+    public static string PrettifyJson(string input)
+    {
+        if (string.IsNullOrEmpty(input))
+        {
+            return string.Empty;
+        }
+
+        JsonElement jsonElement = JsonDocument.Parse(input).RootElement;
+
+        return JsonSerializer.Serialize(jsonElement, new JsonSerializerOptions
+        {
+            WriteIndented = true
+        });
     }
 }
---
+++ tests/Application.Tests.Unit/Helpers/JsonHelperTests.cs
@@ -0,0 +1,89 @@
+﻿using Application.Helpers;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Tests.Unit.Helpers;
+
+public class JsonHelperTests
+{
+    [Theory]
+    [InlineData(@"{""name"":""John"",""age"":30,""city"":""New York""}")]
+    [InlineData(@"[{""name"":""John"",""age"":30,""city"":""New York""}]")]
+    public void IsValidJson_ReturnsTrue_WhenInputIsValid(string input)
+    {
+        // Arrange
+
+        // Act
+        var result = JsonHelper.IsValidJson(input);
+
+        // Assert
+        result.Should().BeTrue();
+    }
+
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    public void IsValidJson_ShouldReturnFalse_WhenValueIsNullOrEmpty(string? value)
+    {
+        // Act
+
+        // Arrange
+        var result = JsonHelper.IsValidJson(value!);
+
+        // Assert
+        result.Should().BeFalse();
+    }
+
+    [Theory]
+    [InlineData(" ")]
+    [InlineData("invalid json")]
+    [InlineData("{name: 'John', age: 30")]
+    [InlineData(@"{""name"":""John"",""age"":30,""city"":""New York""")]
+    [InlineData(@"""name"":""John"",""age"":30,""city"":""New York""}")]
+    [InlineData(@"[{""name"":""John"",""age"":30,""city"":""New York""}")]
+    [InlineData(@"{""name"":""John"",""age"":30,""city"":""New York""}]")]
+    public void IsValidJson_ReturnsFalse_WhenInputIsInvalid(string input)
+    {
+        // Arrange
+
+        // Act
+        var result = JsonHelper.IsValidJson(input);
+
+        // Assert
+        result.Should().BeFalse();
+    }
+    
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    public void PrettifyJson_ReturnsEmptyString_WhenInputIsNullOrEmpty(string? input)
+    {
+        // Arrange
+
+        // Act
+        string result = JsonHelper.PrettifyJson(input!);
+
+        // Assert
+        result.Should().BeEmpty();
+    }
+
+    [Theory]
+    [InlineData("{\"name\":\"John\",\"age\":30,\"city\":\"New York\"}", "{\r\n  \"name\": \"John\",\r\n  \"age\": 30,\r\n  \"city\": \"New York\"\r\n}")]
+    [InlineData("[1, 2, 3]", "[\r\n  1,\r\n  2,\r\n  3\r\n]")]
+    [InlineData("true", "true")]
+    [InlineData("null", "null")]
+    public void PrettifyJson_WhenInputIsValid_ReturnsPrettifiedJson(string input, string expected)
+    {
+        // Arrange
+
+        // Act
+        string result = JsonHelper.PrettifyJson(input);
+
+        // Assert
+        result.Should().Be(expected);
+    }
+}
