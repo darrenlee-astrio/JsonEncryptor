ef269c58d50938c7887f33d87889a9c53d73e49e
Darren <59044608+darrenleeyx@users.noreply.github.com>
2024-04-09 16:06:42 +0800

    Add project files.

---
+++ src/Application/Helpers/JsonHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Helpers;
+
+public static class JsonHelper
+{
+    public static bool IsJson(string value)
+    {
+        if (string.IsNullOrEmpty(value))
+        {
+            return false;
+        }
+
+        return true;
+    }
+}
