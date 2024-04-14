f8fbaafe525bd53d6e144490805133425c842bd9
Darren <59044608+darrenleeyx@users.noreply.github.com>
2024-04-14 20:29:58 +0800

    Added logic to JsonEncryptor with tests

---
+++ src/Application/Abstractions/IJsonEncryptor.cs
@@ -0,0 +1,8 @@
+﻿namespace Application.Abstractions
+{
+    public interface IJsonEncryptor
+    {
+        string Decrypt(string json, List<string> keysToEncrypt, string aesKey, int aesKeySize, string iv);
+        string Encrypt(string json, List<string> keysToDecrypt, string aesKey, int aesKeySize, string iv);
+    }
+}
