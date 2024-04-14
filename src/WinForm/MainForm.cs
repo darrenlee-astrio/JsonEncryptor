e5063934494996e1ff19934c83e0c16c4decdfde
Darren <59044608+darrenleeyx@users.noreply.github.com>
2024-04-14 20:37:20 +0800

    Removed console app and added winforms

---
+++ src/WinForm/MainForm.cs
@@ -0,0 +1,10 @@
+namespace WinForm
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ src/WinForm/Program.cs
@@ -0,0 +1,11 @@
+namespace WinForm;
+
+internal static class Program
+{
+    [STAThread]
+    static void Main()
+    {
+        ApplicationConfiguration.Initialize();
+        System.Windows.Forms.Application.Run(new MainForm());
+    }
+}
